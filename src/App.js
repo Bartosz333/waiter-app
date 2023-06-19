31f87356b1800ccdf9ea9f2126b1536c1160f100
Bartosz Kozaczkiewicz <bkozaczkiewicz@hotmail.com>
2023-06-19 20:48:32 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Container } from 'react-bootstrap';
+const App = () => {
+  return (
+    <Container>
+      <section>
+        <h2 className='mb-4 text-center'>Test bootstrap</h2>
+      </section>
+    </Container>
+  );
+};
+
+export default App;
