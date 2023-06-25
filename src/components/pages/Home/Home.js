8d74900e9777c6518cf993dde6507126a0e3ddcd
Bartosz Kozaczkiewicz <bkozaczkiewicz@hotmail.com>
2023-06-25 07:46:29 +0200

    Server preparation, add server.mjs

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Button, ListGroup, Stack } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { getAllTables } from '../../../redux/tablesRedux';
@@ -8,29 +9,33 @@ export const Home = () => {
   const tables = useSelector(getAllTables);
   const isLoading = !tables;
 
-  if (isLoading) {
-    return <Loading />;
-  }
-
   return (
     <div>
       <h1 className='my-4'>All TABLES</h1>
-      <ListGroup variant='flush'>
-        {tables.map((table) => (
-          <ListGroup.Item key={table.id} status={table.status} className='px-0'>
-            <Stack direction='horizontal' gap={4}>
-              <h2 className='my-2'>Table {table.id}</h2>
-              <p className='my-2'>
-                <strong>Status: </strong>
-                {table.status}
-              </p>
-              <Link className='ms-auto' to={`/table/${table.id}`}>
-                <Button variant='primary'>Show more</Button>
-              </Link>
-            </Stack>
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <ListGroup variant='flush'>
+          {tables.map((table) => (
+            <ListGroup.Item
+              key={table.id}
+              status={table.status}
+              className='px-0'
+            >
+              <Stack direction='horizontal' gap={4}>
+                <h2 className='my-2'>Table {table.id}</h2>
+                <p className='my-2'>
+                  <strong>Status: </strong>
+                  {table.status}
+                </p>
+                <Link className='ms-auto' to={`/table/${table.id}`}>
+                  <Button variant='primary'>Show more</Button>
+                </Link>
+              </Stack>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      )}
     </div>
   );
 };
