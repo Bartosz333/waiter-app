c0b8117971a4611a10b75f19956258063371da3f
Bartosz Kozaczkiewicz <bkozaczkiewicz@hotmail.com>
2023-06-26 08:32:03 +0200

    spelling error correction

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,8 +14,8 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENTION__
-      ? window.__REDUX_DEVTOOLS_EXTENTION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
       : (f) => f
   )
 );
